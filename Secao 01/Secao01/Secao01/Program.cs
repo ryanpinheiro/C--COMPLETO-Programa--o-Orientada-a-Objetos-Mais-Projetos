d8341f92ccc2e76ee550d6e46b3aa718fe02d4f4
Ryan Pinheiro <gabrielryan024@gmail.com>
2024-11-22 17:54:35 -0300

    Primeiro Commit
    Commit feito pela primeira vez com primeiro projeto criado e alguns arquivos do curso

---
+++ Secao 01/Secao01/Secao01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace secao01
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
+
+
